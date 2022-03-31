71d2dfee3fcdd9a8063b7158bfe9c113dbf53442
Dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-31 22:17:29 +0200

    Create EX2_2.c

---
+++ TP1/EX2_2.c
@@ -0,0 +1,30 @@
+/*
+* Retrouver l'essemble des TP sur "https://github.com/dylanPerinetti/ESTACA" 
+* Créé par dylanPerinetti 
+* Le 31/03/2022
+*
+*/
+#include <stdio.h>
+
+int fact(int _x);
+
+int main(void)
+{
+  int n, k,resultat;// Declaration + affectation
+
+  printf("\nVeuillez saisir la valeur de n (pour calculer un coefficient binomial k parmis n ) :\n>_");
+  scanf("%d",&n);           // Saisie utilistaeur
+  printf("\nVeuillez renseigner une valeur de k :\n>_");
+  scanf("%d",&k);
+
+  resultat = fact(n)/(fact(k)*fact(n-k));
+  printf("\nLe coefficient binomial %d parmi %d est : %d\n", k, n, resultat);  // affichage du resultat
+  return 0;                         // return pour le int main()
+}
+
+int fact(int _x)
+{
+  int p,_resultat=1;
+  for(p=1;p<=_x;p++)_resultat = _resultat*p;
+  return _resultat;
+}

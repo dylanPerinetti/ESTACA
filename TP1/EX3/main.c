d4e70345bcb8783dc4f1c3c220fb142240629dd0
Dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-04-01 00:27:00 +0200

    Update main.c

--- TP1/EX3/main.c
+++ TP1/EX3/main.c
@@ -9,9 +9,17 @@
 
 int main(void)
 {
- //declaration des variables
- float distance;
- distance = saisirVoyage();
- printf("La distance du voyage est : %f\n",distance);
- return 0;
+  //declaration des variables
+  float distance;
+  
+  distance = saisirVoyage();
+  
+  printf("La distance du voyage est : %f\n",distance);
+
+  if(distance == 0)printf("A dos de lapin :\n __    __\n/ \\\\..// \\ \n  ( oo )\n   \\__/\n");
+  else if(distance <= 20)printf("A pied :\nOooo.\n(   /\n )  /\n(  /\n –\n");
+  else if(distance<=50)printf("A vélo :\n   __O\n _ \\<,_\n (*)/ (*)\n");
+  else printf("En voiture :\n   _\n__/ \\__\n|      |\n--O--O—\n");
+
+  return 0;
 }

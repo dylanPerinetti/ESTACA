f8729207cc5bbbe0db7a9d10096da5b33d64babf
Dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-31 21:33:36 +0200

    Create EX2_1.c

---
+++ TP1/EX2_1.c
@@ -0,0 +1,24 @@
+/*
+* Retrouver l'essemble des TP sur "https://github.com/dylanPerinetti/ESTACA" 
+* Créé par dylanPerinetti 
+* Le 31/03/2022
+*
+* 1) Les entré et les sortie de ce programme sont n pour stdin et resultat pour sdtout
+*
+*
+*
+*/
+#include <math.h>
+
+int main(void)
+{
+    int n,resultat=1;         // Declaration + affectation
+  
+    printf("Veuillez saisir la valeur de n (pour calculer un coefficient binomial n parmis k ):\n>_");
+    scanf("%d",&n);           // Saisie utilistaeur
+  
+    for(int p=1;p<=n;p++)resultat = resultat*p;// Calacule du Factoriel
+  
+    printf("%d! = %d\n",n,f); // affichage du resultat
+  return 0;                   // return pour le int main()
+}

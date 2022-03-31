faa8577c7cbf33dabacc42b5b82e75b627be8dc6
Dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-31 21:16:51 +0200

    Update EX1_2.c

--- TP1/EX1_2.c
+++ TP1/EX1_2.c
@@ -12,10 +12,10 @@ int main(void)
   printf("\nBonjour !");
   do
   {
-    printf("/nEntrez la somme demandee : ");
+    printf("\nEntrez la somme demandee : ");
     scanf("%d", &saisie_utilisateur);
     saisie_utilisateur >= 100 ? printf("\nRetrait refuse ") : printf("\nRetrait accepte ");
   }while(saisie_utilisateur!=0);
-  printf("Au Revoir !");
+  printf("\nAu Revoir !");
   return 0;
 }
